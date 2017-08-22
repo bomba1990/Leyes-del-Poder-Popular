58146de63bd3240eb4e681b177177065f27b5f70
bomba1990 <marianoramirez353@gmail.com>
2017-08-21 23:48:12 -0400

    1.1.10

--- app/src/main/java/com/publisnet/leyesconcejoscomunales/AppConfig.java
+++ app/src/main/java/com/publisnet/leyesconcejoscomunales/AppConfig.java
@@ -7,5 +7,6 @@ package com.publisnet.leyesconcejoscomunales;
 public class AppConfig {
     public static final long SPLASH_DELAY = 3000;
     public static final String SPLASH_FIRST_TIME = "SPLASH_SHAREDPREFERENCES";
+    public static final String LAST_VERSION = "SPLASH_LAST_VERSION";
     public static final String TAG = "LeyesPoderPopular";
 }
--- app/src/main/java/com/publisnet/leyesconcejoscomunales/activity/SplashActivity.java
+++ app/src/main/java/com/publisnet/leyesconcejoscomunales/activity/SplashActivity.java
@@ -2,6 +2,8 @@ package com.publisnet.leyesconcejoscomunales.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -37,23 +39,36 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        PackageInfo pInfo = null;
+        int version = 0;
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            version = pInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
         sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         boolean first_time  = sharedPref.getBoolean(AppConfig.SPLASH_FIRST_TIME, true);
+        int last_version  = sharedPref.getInt(AppConfig.LAST_VERSION, 0);
+
 
         mainIntent = new Intent(getApplicationContext(), MainActivity.class);
 
-        if(!first_time){
+        if(!first_time && version != last_version){
             startActivity(mainIntent);
             finish();
             return;
         }
 
 
+        final int finalVersion = version;
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putBoolean(AppConfig.SPLASH_FIRST_TIME, false);
+                editor.putInt(AppConfig.LAST_VERSION, finalVersion);
                 editor.apply();
 
                 startActivity(mainIntent);
